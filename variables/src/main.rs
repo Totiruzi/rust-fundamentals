84018a3318445af189ee0fbf819a3d5c5332cf58
Totiruzi <totiruzi@yahoo.com>
2026-06-29 03:52:48 +0300

    Worked With Variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
    let mut x = 5;
    println!("The value of x is: {x}");
@@ -14,4 +16,35 @@ fn main() {
    }
 
    println!("The value of x in the Outer scope is x: {x}");
+
+
+   // Arrays
+   fn array_indexing() {
+      // Invalid Array Index
+
+      let a = [1, 2, 3, 4, 5];
+
+      println!("Please enter an array index");
+
+      let mut index = String::new();
+
+      io::stdin()
+         .read_line(&mut index)
+         .expect("Failed to read line 👽");
+
+         let index: usize = index
+            .trim()
+            .parse()
+            .expect("Index entered was not a number");
+
+      let element = a[index];
+
+      println!("The value of the element at index {index} is: {element}!")
+   }
+
+   // The program "array_indexing" demonstrates what happens when trying to index an 
+   // out of bound element. Entering an index between 0 & 4 won't break the program but
+   // 5 and above 
+   // Uncomment the code below to allow it run.
+   // array_indexing();
 }
